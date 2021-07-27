c46ca870f66edca40ead80a62a822e9c7532cf21
Noman Abdullah <71120198+nomanabdullah-dev@users.noreply.github.com>
2021-07-27 15:54:58 +0600

    completed admin panel

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,5 +6,5 @@ mix.js('resources/js/app.js', 'public/js').vue({ version: 2 })
         //
     ]);
 mix.styles(['public/css/main.css',
-        'public/css/grid.min.css'
-        ], 'public/css/app.css');
+    'public/css/grid.min.css'
+    ], 'public/css/app.css');
